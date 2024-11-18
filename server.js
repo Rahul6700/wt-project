c65ba9bfc341132e8d6b375fa12ac9cf9a4703ca
Rahul Senthil Kumar <rahulsk4087@gmail.com>
2024-11-18 11:14:16 +0530

    more backend

--- server.js
+++ server.js
@@ -1,24 +1,61 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 
-app.get('/test',(req, res) => {
-res.send("hello there!")
-})
+app.get('/', (req, res) => {
+  res.send('hello there!');
+});
 
 app.use(express.json());
 
+app.post('/', (req, res) => {
+  const user = req.body.user;
+  const id = req.body.id;
+  //store user in DB under the correct room_id
+  // if successfull return 1, else return 0 ( return it in a variable called 'join')
+  if (join == 1) {
+    res.status(200).send(req.body.user); // sends back username if successfull
+  } else {
+    res.status(400).send(0); // 0 means room joined successfully
+  }
+});
+
+//sending user guess to db
 app.post('/guess', (req, res) => {
   const id = req.body.id;
   const guess = req.body.guess;
+  //first check room_id, return a varible 'room_id', if room_id == id, return the room_id stored in db, else return 0
   //check with db matching
-})
+  //store yes or no in a variable, say "ans" (1 for yes, 0 for no)
 
+  //just for demo
+  const room_id = 100;
+  const ans = 'china';
+
+  if (id != room_id) {
+    res.status(404).send(2); //2 means invalid room
+    return;
+  } else {
+    if (ans == guess) {
+      res.status(200).send(1); // 1 means correct guess
+      return;
+    } else {
+      res.status(400).send(0); // 0 means wrong guess
+    }
+  }
+});
+
+//image fetching from db
 app.post('/fetch', (req, res) => {
   const id = req.body.id;
   //need to fetch a random img from db
-})
+});
 
-app.listen(port, ()=>{
-  console.log(`backend listening on ${port}`)
-})
+//function to show connected users in the room
+app.post('/', (req, res) => {
+  const id = req.body.id;
+});
+
+app.listen(port, () => {
+  console.log(`backend listening on ${port}`);
+});
